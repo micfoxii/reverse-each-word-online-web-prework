541e66458fa37c3224861939580839aff7d412eb
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-22 21:30:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
-  string_array = string.split
+  string_array = string.split(" ")
   reverse_array = string_array.each {|x| x.reverse!}
   return reverse_array.join
 end
