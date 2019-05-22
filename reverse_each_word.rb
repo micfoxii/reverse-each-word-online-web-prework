b435b89fee92fce864b8a6bd7468399f46aff7d7
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-22 21:35:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(string)
-  string_array = string.split(" ")
-  reverse_array = string_array.each {|x| x.reverse!}
-  return reverse_array.join(" ")
-end
+#def reverse_each_word(string)
+#  string_array = string.split(" ")
+#  reverse_array = string_array.each {|x| x.reverse!}
+#  return reverse_array.join(" ")
+#end
 
 def reverse_each_word(string)
   array.split(" ").collect{|x| x.reverse!}.join(" ")
